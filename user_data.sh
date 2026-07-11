186e4602f53bed912872bb8a506603b27f2ce595
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-07-11 09:36:23 +0530

    all

--- user_data.sh
+++ user_data.sh
@@ -37,3 +37,12 @@ sudo chmod +x /opt/kubectx/kubens
 kubens
 # K9s is a terminal-based UI for Kubernetes. Instead of typing many kubectl commands, you get an interactive dashboard.
 curl -sS https://webinstall.dev/k9s | bash
+
+
+# Install Docker (simple & reliable)
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
